a63357b550f516c8d6abb69ac2a4d4bffcad75a9
pavel <pvorona54@gmail.com>
2022-10-23 12:30:08 -0400

    Add project files.

---
+++ Homework2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.FormattingEnabled = true;
+            this.n1.Items.AddRange(new object[] {
+            "-8",
+            "-7",
+            "-6",
+            "-5",
+            "-4",
+            "-3",
+            "-2",
+            "-1",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.n1.Location = new System.Drawing.Point(167, 140);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(151, 28);
+            this.n1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.FormattingEnabled = true;
+            this.lblOutput.ItemHeight = 20;
+            this.lblOutput.Location = new System.Drawing.Point(468, 196);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(150, 104);
+            this.lblOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox n1;
+        private Label label1;
+        private Button button1;
+        private ListBox lblOutput;
+    }
+}
---
+++ Homework2/Form1.cs
@@ -0,0 +1,35 @@
+namespace Homework2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string iresult;
+            int iEnd,  iCounter, strEnd, inumber;
+            double inumberpow;
+            strEnd = 0;
+            iEnd = 10;
+            lblOutput.Items.Clear();
+            inumber = Convert.ToInt32(n1.Text);
+            iCounter = strEnd;
+            while (iCounter <= iEnd)
+            {
+                inumberpow = Math.Pow(inumber, iCounter);
+                iresult = inumber.ToString() + " pow " + iCounter.ToString() + " = " + inumberpow.ToString();
+                //This is the string that will by added to the
+                //For example, if the user inputs 5, the first
+                //value will be “5 * 1 = 5”
+                lblOutput.Items.Add(iresult);
+                iCounter = iCounter + 1; // alternatively you can write iCounter ++
+
+            }
+
+            
+        }
+    }
+}
